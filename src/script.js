ba01f5e6506aac9029fcebecab5ab942ee7e7133
PAZHames <phoebehames292@gmail.com>
2023-10-16 13:14:37 +0100

    added initial retieveResult function
    JS: created retrieveResult function to access both APIs
    converted json into string for pushing into p (created dynamically in pushToPage function)
    found that chosen API has errors - rethink incoming

--- src/script.js
+++ src/script.js
@@ -1,4 +1,6 @@
 const dummyContent = "stuff";
+const shakespeareURL = ("https://api.funtranslations.com/translate/shakespeare.json")
+
 
 const result1Container = document.getElementById("translation1Container");
 const result2Container = document.getElementById("translation2Container");
@@ -16,5 +18,26 @@ function pushToPage(content, container) {
   container.append(resultText);
 }
 
-pushToPage(dummyContent, result1Container);
+
+
+
+
+async function retrieveResult (method, url, contentType, requestBody) {
+  const response = await fetch(`${url}`, {
+  method: `${method}`,
+  headers: {
+    // "Content-Type": `${contentType}`
+    "Host": "api.funtranslations.com"
+  },
+  body: JSON.stringify(`${requestBody}`)
+ })
+  
+ const resultObject = await response.json();
+ const resultText = resultObject.contents.translated;
+  // .then(response => response.json())
+  // .then(let result = (response.contents.translated))
+  return resultText;
+}
+
+pushToPage(retrieveResult("POST", shakespeareURL, "text", "this is an example translation"), result1Container);
 pushToPage(dummyContent, result2Container);
