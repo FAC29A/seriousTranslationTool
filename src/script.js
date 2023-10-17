bc1bdcb3a776eef99df1f51e4b543d0ac22264bd
Jason Warren <contact.jwarren@gmail.com>
2023-10-17 16:14:53 +0100

    API Wranglin'! Now works down to line 65 Got confirmation of a successful API fetch from the API host!!!! Committing now, but more to do

--- src/script.js
+++ src/script.js
@@ -1,11 +1,15 @@
 //API Sources
   const host = "api.funtranslations.com";
-  const shakespeare = ("https://api.funtranslations.com/translate/shakespeare?=");
+  const shakespeare = ("https://api.funtranslations.com/translate/shakespeare.json?text=");
+  //* Think this was formatted incorrectly. By me.
 
 //HTML Elements
   const container1 = document.getElementById("result1Box");
+  console.log("Container 1 is: ", container1)
   const container2 = document.getElementById("result2Box");
+  console.log("Container 2 is: ", container2)
   const form = document.querySelector("form");
+  console.log("Form is: ", form)
 
 //Functions
   //Get User Input
@@ -13,7 +17,10 @@
   form.addEventListener("submit", async (event) => {
     event.preventDefault();
     const formData = new FormData(form);
-     await pullFromAPI(shakespeare, "POST", "application/JSON", formData, container1);
+  
+    await pullFromAPI(shakespeare, "GET", "application/JSON", formData, container1);
+    //* I changed POST to GET
+    //* We were getting the forbidden response because the API doesn't accept POST
   })
 
   //Get Stuff from API
@@ -21,28 +28,51 @@
       
       // Turn form text input into string and create full URL
       const formDataObject = Object.fromEntries(requestBody.entries());
+      console.log("formDataObject is: ", formDataObject);
+    
       const url = api;
-      const queryString = new URLSearchParams(formDataObject).toString();
-      const fullURL = url + "?" + queryString;
 
-      console.log(fullURL);
+      //// const queryString = new URLSearchParams(formDataObject.originalTextInput).toString();
+      //// console.log("queryString is: ", queryString);
+      //! This is returning originalTextInput=Let%27s+try+this...
+      //todo but we want Let%27s+try+this...
+      
+      // Several Minutes Later
+
+      //* Okay so I think URLSearchParams is way more advanced than what we need, I think it's the basis for iterating through a shitload of big multi-value objects and extracting search terms from them
+      //* SO let's try...
+      const queryString = formDataObject.originalTextInput.toString();
+      console.log("queryString is: ", queryString);
+
+      //// const fullURL = url + "?" + queryString;
+      //* That question mark is already present in the shakespeare CONST
+      const fullURL = url + queryString;
+      console.log("fullURL is: ", fullURL);
 
       const translatedText = fetch(fullURL, {
         method: method,
-        // headers: {
-        //   // "Content-Type": contentType,
-        //   "Host": host,
-        // },
-        body: JSON.stringify(formDataObject)
+        //// headers: {
+        //// "Content-Type": contentType,
+        ////   "Host": host,
+        //// },
+        //// body: JSON.stringify(formDataObject)
       })
       .then((response) => {
         if (!response.ok) {
           const error = new Error(response.status);
           throw error;
         }
-        return response.JSON;
+        console.log("Raw Response is: ", response);
+        //* OKAY THIS CONSOLE LOG IS NOW SHOWING ME A SUCCESSFUL CALL!!!!
+
+        //* Phoebe's Original
+          //? console.log(response.JSON);
+          //? return response.JSON;
+        //* Attempt 1
+          console.log("Stringified Response is: ", JSON.stringify(response));
+          return JSON.stringify(response);
+        //todo Okay
       })
-      // .then((responseJSON) => JSON.parse(responseJSON)) -- commented out as now happening further up
       .then((responseObject) => responseObject.contents.translated)
       .catch(console.error)
 
